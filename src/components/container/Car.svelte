761089931da924b7e67a9fd4617acf870795cd68
Enzuo <potc.zon.e+git@gmail.com>
2022-04-30 23:15:39 +0200

    Add graph for car speed

--- src/components/container/Car.svelte
+++ src/components/container/Car.svelte
@@ -1,7 +1,13 @@
 <script>
+  import {onMount} from 'svelte'
+  // import Plotly from 'plotly.js-dist'
+  import uPlot from 'uplot'
+  import 'uplot/dist/uPlot.min.css'  
   import * as cardata from '@/logic/cardata'
   import {Simulation} from '@/logic/simulation2'
   import * as Car from '@/logic/carphysics/carEntity'
+
+
 
   let carId = 'renault_zoe'
 
@@ -23,6 +29,12 @@
     time = t
     engineRpm = carEntity.state.engineRpm
     speed = carEntity.state.speed
+
+    // update chart
+    // Plotly.extendTraces(chartEl, {y: [[speed*3.6]]}, [0])
+    chartData[0].push(t/1000)
+    chartData[1].push(speed*3.6)
+    plotChart.setData(chartData)
   })
 
   function handleStart() {
@@ -34,6 +46,71 @@
 
 
   console.log(carSpecs, carEntity)
+
+  function getData() {
+    return Math.random()
+  }
+
+  const optsUplotChart = {
+    title: "Speed",
+    width: 300,
+    height: 300,
+    cursor: {
+      drag: {
+        setScale: false,
+      }
+    },
+    select: {
+      show: false,
+    },
+    series: [
+      {},
+      {
+        label: "Km/h",
+        scale: "kmh",
+        value: (u, v) => v == null ? "-" : v.toFixed(1),
+        stroke: "blue",
+      },
+    ],
+    scales: {
+      "x": {
+        time: false,
+      },
+      'kmh': {
+        // range: [0, 150],
+      }
+    },
+    axes: [
+      {},
+      {
+        space: 60,
+        scale: 'kmh',
+        values: (u, vals, space) => vals.map(v => +v.toFixed(1)),
+        side: 1,
+        grid: {show: false},
+      }
+    ],
+  };
+
+
+  let chartEl
+  let plotChart
+  let chartData = [
+    [],[]
+      // [0, 100],    // x-values (timestamps)
+      // [        35,         71],    // y-values (series 1)
+      // [        90,         15],    // y-values (series 2)
+    ];
+  onMount(() => {
+		chartEl = document.getElementById('chart');				
+    // Plotly.newPlot(chartEl, [{
+    //   y:[getData()],
+    //   type:'line'
+    // }])
+
+
+    plotChart = new uPlot(optsUplotChart, chartData, chartEl);
+  });
 
 </script>
 
@@ -52,3 +129,4 @@ Car : {carSpecs.name}
 <div>
   Engine rpm : {Math.floor(engineRpm)}
 </div>
+<div id="chart"></div>
