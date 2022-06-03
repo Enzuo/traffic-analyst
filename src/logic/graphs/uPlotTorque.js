cde9d08ffd901d40725fc7ed04f31261d1aab05a
Enzuo <potc.zon.e+git@gmail.com>
2022-06-03 13:00:19 +0200

    [TorqueGraph] Use different functions to either show keys or fill
    The fill graph will show more accuratly what's beeing used in the simulation
    while the key represent only the data points used to define the torque curve
    
    Key graph could be used for editing

--- src/logic/graphs/uPlotTorque.js
+++ src/logic/graphs/uPlotTorque.js
@@ -34,14 +34,7 @@ export function createGraph(cars, element, opts = {}){
   }
 
 
-  function prepareData(cars){
-    // let minRPM = 0
-    // let allCarsRPM = cars.map( car => { 
-    //   let maxX = car.props.engine.torqueX[car.props.engine.torqueX.length-1]
-    //   return maxX
-    // })
-    // let maxRPM = Math.max(...allCarsRPM)
-
+  function getXKeys(cars){
     let torqueXArray = cars.map(car => {
       let xMultiplier = car.props.engine.torqueXMultiplier || 1
       return car.props.engine.torqueX.map(x => x * xMultiplier)
@@ -54,29 +47,66 @@ export function createGraph(cars, element, opts = {}){
     })
     rpms.sort((a,b) => a - b)
 
+    return rpms
+  }
+
+  function getXFill(cars, step = 500){
+    let minRPM = 0
+    let allCarsRPM = cars.map( car => { 
+      let xMultiplier = car.props.engine.torqueXMultiplier || 1
+      let maxX = car.props.engine.torqueX[car.props.engine.torqueX.length-1] * xMultiplier
+      return maxX
+    })
+    let maxRPM = Math.max(...allCarsRPM)
+
+    let rpms = []
+    for(var i=minRPM; i<maxRPM; i+=step) {
+      rpms.push(i)
+    }
+
+    return rpms
+  }
+
+  function getYKey(car, rpm){
+    let engine = car.props.engine
+    let xMultiplier = engine.torqueXMultiplier || 1
+    let index = engine.torqueX.findIndex(x => x * xMultiplier === rpm)
+    if(index >= 0 ){
+      let torque = engine.torqueY[index]
+      return { torque, power :  torqueToKW(torque, rpm)}
+    }
+    return { torque : null, power : null}
+  }
+
+  function getYFill(car, rpm){
+    let engine = car.props.engine
+    let xMultiplier = engine.torqueXMultiplier || 1
+    let torque = getTorqueForRPM(engine.torqueCurve, rpm)
+    let power = torqueToKW(torque, rpm)
+    return {torque, power}
+  }
+
+  function prepareData(cars){
+
+    let rpms = getXFill(cars)
+
+
     let data = [rpms]
     // generate curves data
     for(var i=0; i<cars.length; i++) {
       let car = cars[i]
       let engine = car.props.engine
       let xMultiplier = engine.torqueXMultiplier || 1
-      let torque = []
-      let hp = []
+      let torqueArray = []
+      let powerArray = []
       for(let j=0; j<rpms.length; j++) {
         let rpm = rpms[j]
-        let index = engine.torqueX.findIndex(x => x * xMultiplier === rpm)
-        if(index >= 0 ){
-          let t = engine.torqueY[index]
-          torque.push(t)
-          hp.push(torqueToKW(t, rpm))
-        }
-        else {
-          torque.push(null)
-          hp.push(null)
-        }
+        let {torque, power} = getYFill(car, rpm)
+        torqueArray.push(torque)
+        powerArray.push(power)
       }
-      data.push(torque)
-      data.push(hp)
+      data.push(torqueArray)
+      data.push(powerArray)
     }
 
     return data
