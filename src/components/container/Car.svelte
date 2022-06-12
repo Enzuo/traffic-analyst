23f46e44e9b51213f5a9a87b5f6192a01e81e499
Enzuo <potc.zon.e+git@gmail.com>
2022-06-12 21:56:09 +0200

    Add citroen ln model

--- src/components/container/Car.svelte
+++ src/components/container/Car.svelte
@@ -6,7 +6,7 @@
   import UPlotTorque from './UPlotTorque.svelte'
 
 
-  let carIds = ['dacia_logan', 'peugeot_206', 'renault_trafic2']
+  let carIds = ['dacia_logan', 'peugeot_206', 'renault_trafic2', 'citroen_ln']
   let colors = ["blue","red","orange","green","purple"]
   let colors3D = [0x5895FF,0xFF5758,0xFFA132,"green","purple"]
 
