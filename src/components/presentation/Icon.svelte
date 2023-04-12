109c9ca3e8b1c9a94884de256c49d21fe46ed363
Enzuo <potc.zon.e+git@gmail.com>
2023-04-12 17:51:10 +0200

    Add icomoons icon

---
+++ src/components/presentation/Icon.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let name
+
+
+
+</script>
+
+<svg class="icon icon-{name}"><use xlink:href="symbol-defs.svg#icon-{name}"></use></svg>
+
+
+<style>
+  .icon {
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    stroke-width: 0;
+    stroke: currentColor;
+    fill: currentColor;
+  }
+</style>
