b14fde4f84dac68733b3e18e36cf375f83c5f072
Enzuo <potc.zon.e+git@gmail.com>
2024-07-13 18:17:30 +0200

    Prepare new data structure type

---
+++ src/logic/cardata/types.js
@@ -0,0 +1,105 @@
+/**
+* @typedef {object} CarData
+* @property {string} name
+* @property {string} brand
+* @property {string} trim
+* @property {string} year // production year range 2010-2020
+* @property {number} unitsProduced
+* @property {string} price
+* @property {string} keywords // related, keys, words
+*
+* ENGINES
+* @property {engine} engine
+*
+* TRANSMISSION
+* @property {gearbox|gearbox[]} gearbox
+* @property {string} type // transmission type AWD, RWD, FWD
+* @property {number} wheelDiameter // wheel diameter in cm
+*
+* CONFIGS
+* list of avaiablable configs
+* @property {config[]} configs
+*
+* DIMENSIONS
+* @property {number} weight // kg
+* @property {number} length // mm
+* @property {number} width // mm
+* @property {number} height // mm
+* @property {number} wheelbase // mm
+* @property {number} clearance // mm
+* @property {number} dragCoef
+* @property {number} dragArea // m2
+* @property {string} bodyType // suv, hatchback, sedan, ...
+
+* STATS
+* @property {number} trunk // trunk size in L
+* @property {number} trunkMax // max trunk size in L
+* @property {number} seats
+* @property {number} doors
+* @property {number} acc0100 // constructor stats
+* @property {number} vmax // constructor vmax
+* @property {number} avgConsumption // constructor avg consumption
+* @property {number} energyTank // L or KW
+* @property {number} autonomy // avg autonomy on one energy tank in km
+* @property {number} turningCircle // m
+* @property {string} wheelType //
+*
+* PLANE STATS
+* @property {number} groundRoll
+* @property {number} takeOffSpeed
+* @property {number} wingArea
+* @property {number} liftDragRatio
+*
+* 3D
+* @property {string} model
+* @property {string} modelWheel
+* @property {string} modelMisc
+* @property {number} wheelScale
+* @property {Array} modelLights
+*
+* @property {trims[]} trims
+* @property {options[]} options
+* @property {engine[]} engines list all additional available engines
+* @property {gearbox[]} gearboxes list all additional available gearboxes
+* @property {string} id
+*/
+
+/**
+* @typedef {object} engine
+* @property {string} name
+* @property {number} hp
+* @property {string} spec // 60hp@5000 130nm@3000
+* @property {Array<number>} torqueY
+* @property {Array<number>} torqueX
+* @property {string} type // petrol, diesel
+* @property {string} layout // v8, l4
+* @property {number} displacement // cm3
+*/
+
+/**
+* @typedef {object} gearbox
+* @property {string} name
+* @property {Array<number>} ratio
+* @property {number|Array<number>} reverseRatio
+* @property {number|Array<number>} gearTransfer // additional HI-LOW gear ratios
+* @property {number} driveRatio
+*/
+
+/**
+* @typedef {object} trims
+* @property {?} trim
+* @property {config[]} configs
+* ... all other values possible to ovveride
+*/
+
+/**
+* @typedef {object} options
+* @property {?} name
+*/
+
+/**
+ * @typedef {object} config
+ * @property {string} engine engine name
+ * @property {string} gearbox gearbox name
+ * ... and all other values possible to ovveride, default values -> trims values -> config values
+ */
