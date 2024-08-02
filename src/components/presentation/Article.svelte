e0c95929c3a846ae3e09ae20cffa9ec575b0a4f9
Enzuo <potc.zon.e+git@gmail.com>
2024-08-03 01:13:59 +0200

    Update prettier for svelte

--- src/components/presentation/Article.svelte
+++ src/components/presentation/Article.svelte
@@ -5,26 +5,20 @@
   export let image
 </script>
 
-
 <div class="article">
-  <h1>{ title }</h1>
-  <img src={'images/'+image}>
-  <p class="date">on: { date }</p>
-  <p class="date">by: { author }</p>
+  <h1>{title}</h1>
+  <img src={'images/' + image} />
+  <p class="date">on: {date}</p>
+  <p class="date">by: {author}</p>
   <slot>
     <!-- the mdsvex content will be slotted in here -->
   </slot>
 </div>
 
-
 <style>
-
-.article {
-    border:2px solid var(--primary);
-    margin:24px;
-    padding:24px;
+  .article {
+    border: 2px solid var(--primary);
+    margin: 24px;
+    padding: 24px;
   }
-
-
-
 </style>
