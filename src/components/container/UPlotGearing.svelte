85a12823880da8ef9b3db8cf5a7bb4bec4ba0cb1
Enzuo <potc.zon.e+git@gmail.com>
2024-08-14 10:36:13 +0200

    Handle graph errors, display an error icon

--- src/components/container/UPlotGearing.svelte
+++ src/components/container/UPlotGearing.svelte
@@ -1,15 +1,24 @@
 <script>
   import {createGraph} from '@/logic/graphs/uPlotGearing'
+  import Icon from '../presentation/Icon.svelte'
 
   export let car
   export let showForce=true
   let chartElement
+  let hasError
 
   $ : if(chartElement) {
     chartElement.innerHTML = ''
-    createGraph(car, chartElement, {isGearForce:showForce})
+    try {
+      createGraph(car, chartElement, {isGearForce:showForce})
+      hasError = false
+    }
+    catch(e){
+      hasError = true
+    }
   }
 
 </script>
 
 <div bind:this={chartElement}></div>
+{#if hasError}<div class="graph-error">!<Icon name="wrench"></Icon></div>{/if}
--- src/components/container/UPlotTorque.svelte
+++ src/components/container/UPlotTorque.svelte
@@ -1,17 +1,26 @@
 <script>
   import { onMount } from 'svelte'
   import { createGraph } from '@/logic/graphs/uPlotTorque'
+  import Icon from '../presentation/Icon.svelte'
 
   export let cars
   export let opts = {}
 
   let chartElement
+  let hasError
 
   $ : if(chartElement) {
     chartElement.innerHTML = ''
-    createGraph(cars, chartElement, opts)
+    try {
+      createGraph(cars, chartElement, opts)
+      hasError = false
+    }
+    catch(e){
+      hasError = true
+    }
   }
 
 </script>
 
 <div bind:this={chartElement}></div>
+{#if hasError}<div class="graph-error">!<Icon name="wrench"></Icon></div>{/if}
