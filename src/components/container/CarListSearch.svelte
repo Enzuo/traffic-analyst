65944e8f5b7dac563b8b8195d2250f0e977c3458
Enzuo <potc.zon.e+git@gmail.com>
2024-09-01 01:13:57 +0200

    Add sort option

--- src/components/container/CarListSearch.svelte
+++ src/components/container/CarListSearch.svelte
@@ -3,11 +3,20 @@
   import Icon from '@/components/presentation/Icon.svelte'
   import SearchBar from './SearchBar.svelte'
   import CarList from '@/components/presentation/CarList.svelte'
+  import SortSelector from './SortSelector.svelte'
+  import { parseDate } from '@/logic/cardata'
 
   export let cars=[]
   export let selectedCarId=null
 
+  let sortOptions = ['name', 'year', 'price']
+  let sortBy = sortOptions[0]
+
   const dispatch = createEventDispatcher()
+
+  $: sortFn = getSortFn(sortBy)
+  $: carsSorted = cars.toSorted(sortFn)
+  $: foo = "hello" + sortFn
 
 
   function handleCarClick (e) {
@@ -21,8 +30,38 @@
     })
   }
 
+  function handleSortSelect(event) {
+    console.log(event)
+    sortBy = event.detail
+  }
+
+  // Utils
+
+  function getSortFn(sortBy){
+    console.log('getSortFn', sortBy)
+    if(sortBy === 'name'){
+      return (a, b) => {
+        return a.name - b.name
+      }
+    }
+
+    if(sortBy === 'year'){
+      return (a, b) => {
+        return parseDate(a.year)[0] - parseDate(b.year)[0]
+      }
+    }
+
+    if(sortBy === 'price'){
+      return (a, b) => {
+        return a.price - b.price
+      }
+    }
+  }
+
 </script>
 
 
 <SearchBar on:search ></SearchBar>
-<CarList cars={cars} selectedCarId={selectedCarId} on:click={handleCarClick}></CarList>
+<SortSelector options={sortOptions} currentSort={sortBy} on:select={handleSortSelect}></SortSelector>
+{foo}
+<CarList cars={carsSorted} selectedCarId={selectedCarId} on:click={handleCarClick}></CarList>
--- src/logic/cardata/index.js
+++ src/logic/cardata/index.js
@@ -240,3 +240,26 @@ const defaultCar = {
     driveRatio : 4,
   }
 }
+
+
+/**
+ *
+ * @param {string|number} date
+ */
+export function parseDate(date) {
+  if(!date){
+    return [null, null]
+  }
+
+  if(typeof date === 'number'){
+    return [date, date]
+  }
+
+  const years = date.match(/\d+/g).map(y => parseInt(y, 10))
+
+  if(years.length < 2){
+    return [years[0], years[0]]
+  }
+
+  return years[0] > years[1] ? [years[1], years[0]] : [years[0], years[1]]
+}
