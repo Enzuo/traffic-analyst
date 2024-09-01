7daa389c68c9c312e8d95288d68e3e7f980c1c58
Enzuo <potc.zon.e+git@gmail.com>
2024-09-01 22:37:55 +0200

    Fix main page style on mobile

--- src/components/presentation/Article.svelte
+++ src/components/presentation/Article.svelte
@@ -6,25 +6,36 @@
 </script>
 
 <div class="article">
-  <h1>{title}</h1>
-  <img src={'images/' + image} />
-  <p class="date">{date}</p>
-  <slot>
-    <!-- the mdsvex content will be slotted in here -->
-  </slot>
+  <div class="article-container">
+    <h1>{title}</h1>
+    <img src={'images/' + image} />
+    <p class="date">{date}</p>
+    <slot>
+      <!-- the mdsvex content will be slotted in here -->
+    </slot>
+  </div>
 </div>
 
 <style>
   .article {
     background: rgba(255,255,255,0.9);
     max-width: 960px;
-    border: 2px solid var(--primary);
-    margin: 24px;
     padding: 24px;
+  }
+
+  @media (min-width: 960px) {
+    .article {
+      margin: 24px;
+      border: 2px solid var(--primary);
+    }
   }
 
   .date {
     margin: 0.5em;
     color: var(--secondary)
+  }
+
+  img {
+    width:100%;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import Tools from '@/components/tools/Tools.svelte'
   import TrafficSim from '@/components/container/TrafficSim.svelte'
   import Article from '@/components/presentation/Article.svelte'
-  import TestProcedural from '@/components/tools/TestProcedural.svelte';
+  import TestProcedural from '@/components/tools/TestProcedural.svelte'
   import TrafficSimPixelBanner from '@/components/container/TrafficSimPixelBanner.svelte'
   import createTrafficSimulation from '@/logic/trafficsim/trafficSimulation'
   import { onMount, onDestroy } from 'svelte'
@@ -22,7 +22,7 @@
   })
 
   onDestroy(() => {
-    if(simulation){
+    if (simulation) {
       simulation.stop()
     }
   })
@@ -30,10 +30,12 @@
 
 <main>
   <TrafficSimPixelBanner traffic_sim={simulation}></TrafficSimPixelBanner>
-  <DebugPerformanceGraph debugPerf={debugPerf} top="130px"></DebugPerformanceGraph>
-  <h1>
-    <img src="title8.png" alt="AutoWaves"/>
-  </h1>
+  <DebugPerformanceGraph {debugPerf} top="130px"></DebugPerformanceGraph>
+  <div class="app-title">
+    <h1>
+      <img src="title8.png" alt="AutoWaves" />
+    </h1>
+  </div>
   <div id="tools">
     <Tools></Tools>
   </div>
@@ -46,67 +48,74 @@
   </div>
   <div id="articles">
     {#each data.articles as article}
-    <Article {...article.frontmatter}>
-      {@html article.content}
-    </Article>
+      <Article {...article.frontmatter}>
+        {@html article.content}
+      </Article>
     {/each}
   </div>
   <BackgroundCanvas></BackgroundCanvas>
 </main>
 
 <style>
-	main {
-		/* text-align: center; */
-		/* max-width: 240px; */
-		margin: 0 auto;
+  main {
+    /* text-align: center; */
+    /* max-width: 240px; */
+    margin: 0 auto;
     display: flex;
     flex-direction: column;
     align-items: center;
-	}
+    overflow: hidden;
+  }
 
   #tools {
     width: 100%;
-    display:none;
+    display: none;
   }
 
   .button {
     box-shadow: 1px 1px 0px 2px var(--secondary);
-    margin:1.5em;
+    margin: 1.5em;
   }
 
   #footer {
-    color:var(--secondary);
+    color: var(--secondary);
   }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  .app-title {
+    width: 100%;
     position: absolute;
     top: 75px;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-  h1 {
-    color: #A2DED0; /* Mint Green */
-    color: #F7A9A8; /* Rose Pink*/
-    color: #F8E9A1; /* Pale Yellow*/
-
-    color:#FFA7C4 ;
-    color:#C3FFB5 ;
-    color:#E5F6FF;
-    color:#C2D8F9 ;
-    color:#5F5AA2 ;
-    color :#fff7de;
+    text-align: center;
   }
 
   h1 img {
-    width:448px;
+    width: 448px;
+    max-width:100%;
     image-rendering: pixelated;
   }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+  h1 {
+    color: #a2ded0; /* Mint Green */
+    color: #f7a9a8; /* Rose Pink*/
+    color: #f8e9a1; /* Pale Yellow*/
+
+    color: #ffa7c4;
+    color: #c3ffb5;
+    color: #e5f6ff;
+    color: #c2d8f9;
+    color: #5f5aa2;
+    color: #fff7de;
+  }
+
 </style>
