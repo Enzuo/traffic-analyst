8c005050921598631e47b9031b408c7ca6fa7196
Enzuo <potc.zon.e+git@gmail.com>
2024-09-21 20:28:52 +0200

    Add default rim

--- src/logic/3DsceneGraph/WheelProcedural.ts
+++ src/logic/3DsceneGraph/WheelProcedural.ts
@@ -1,21 +1,44 @@
 import * as THREE from 'three'
+import { loadModel } from './loader'
 
 export class WheelProcedural extends THREE.Object3D {
   public object: THREE.Mesh
+  public objectRim
+
+  public wheelDiameter = 70
+  public treadWidth = 20
+  public rimDiameter = 40
+  public bevelSize = 2
 
   constructor(wheelModel: THREE.Mesh) {
     super()
-
+    this.addRim()
     this.object = wheelModel.clone()
+    //this.resize()
     this.add(this.object)
   }
 
-  resize(wheelDiameter = 70, treadWidth = 20, rimDiameter = 40, bevelSize = 2) {
+  async addRim(){
+    const rimGLB = await loadModel('rimDefault')
+    const objectRim = rimGLB.scene.children[0]
+    this.objectRim = objectRim
+    this.add(objectRim)
+    this._resize()
+  }
+
+  resize(wheelDiameter = 70, treadWidth = 20, rimDiameter = 40) {
+    this.wheelDiameter = wheelDiameter
+    this.treadWidth = treadWidth
+    this.rimDiameter = rimDiameter
+    this._resize()
+  }
+
+  _resize(){
     let geometry: THREE.BufferGeometry = this.object.geometry
     let positionAttribute = geometry.getAttribute('position') as THREE.BufferAttribute
     let colorAttribute = geometry.getAttribute('color') as THREE.BufferAttribute
 
-    console.log('resize wheel', wheelDiameter, treadWidth, rimDiameter)
+    console.log('resize wheel', this.wheelDiameter, this.treadWidth, this.rimDiameter)
     const unitScale = 0.01
     const green = new THREE.Color(0, 1, 0)
     const yellow = new THREE.Color(1, 1, 0)
@@ -30,10 +53,10 @@ export class WheelProcedural extends THREE.Object3D {
       // Tread (rolling band)
       if (color.equals(new THREE.Color(THREE.Color.NAMES.red))) {
         const side = Math.sign(vertex.y)
-        vertex.y = (treadWidth / 2 - bevelSize) * side * unitScale
+        vertex.y = (this.treadWidth / 2 - this.bevelSize) * side * unitScale
 
         const vertexVec = new THREE.Vector2(vertex.x, vertex.z).normalize()
-        vertexVec.multiplyScalar((wheelDiameter / 2) * unitScale)
+        vertexVec.multiplyScalar((this.wheelDiameter / 2) * unitScale)
         vertex.x = vertexVec.x
         vertex.z = vertexVec.y
       }
@@ -41,29 +64,29 @@ export class WheelProcedural extends THREE.Object3D {
       // Sidewall
       if (color.equals(new THREE.Color(THREE.Color.NAMES.blue))) {
         const side = Math.sign(vertex.y)
-        vertex.y = (treadWidth / 2) * side * unitScale
+        vertex.y = (this.treadWidth / 2) * side * unitScale
 
         const vertexVec = new THREE.Vector2(vertex.x, vertex.z).normalize()
-        vertexVec.multiplyScalar((wheelDiameter / 2 - bevelSize) * unitScale)
+        vertexVec.multiplyScalar((this.wheelDiameter / 2 - this.bevelSize) * unitScale)
         vertex.x = vertexVec.x
         vertex.z = vertexVec.y
       }
 
       if (color.equals(green)) {
-        vertex.y = (treadWidth / 2) * unitScale
+        vertex.y = (this.treadWidth / 2) * unitScale
 
         const vertexVec = new THREE.Vector2(vertex.x, vertex.z).normalize()
-        vertexVec.multiplyScalar((rimDiameter / 2) * unitScale)
+        vertexVec.multiplyScalar((this.rimDiameter / 2) * unitScale)
         vertex.x = vertexVec.x
         vertex.z = vertexVec.y
       }
 
       // Inner bevel
       if (color.equals(yellow)) {
-        vertex.y = (treadWidth / 2 - bevelSize * 5) * unitScale
+        vertex.y = (this.treadWidth / 2 - this.bevelSize * 5) * unitScale
 
         const vertexVec = new THREE.Vector2(vertex.x, vertex.z).normalize()
-        vertexVec.multiplyScalar((rimDiameter / 2 - bevelSize) * unitScale)
+        vertexVec.multiplyScalar((this.rimDiameter / 2 - this.bevelSize) * unitScale)
         vertex.x = vertexVec.x
         vertex.z = vertexVec.y
       }
@@ -74,5 +97,25 @@ export class WheelProcedural extends THREE.Object3D {
 
     positionAttribute.needsUpdate = true
     this.object.geometry.attributes.position.needsUpdate = true
+
+    // Adjust rim on the wheel
+    if(this.objectRim){
+      this.objectRim.position.set(this.treadWidth/2 * unitScale, 0, 0)
+      const scale = calcWheelScale(this.rimDiameter, this.objectRim)
+      const isRightWheel = false
+      this.objectRim.scale.set(isRightWheel ? -scale : scale, scale, scale)
+    }
   }
+}
+
+
+/**
+ *
+ * @param {number} wheelDiameter {cm}
+ * @param {THREE.Object3D} wheel
+ * @returns {number} scale
+ */
+function calcWheelScale(wheelDiameter, wheel){
+  const modelDiameter = wheel.geometry.boundingBox.max.z * 2 * 100
+  return wheelDiameter ? wheelDiameter/modelDiameter : 1
 }
